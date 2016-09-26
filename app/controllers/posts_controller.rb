448b02d001ac2f0cd6ea2c489bdb054e03c0c79d
MerveDnmz <mervedonmez99@gmail.com>
2016-09-26 11:35:09 +0300

    Add haml file on show and index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 	before_action :authenticate_user!, except: [:index, :show]
-	def index 
+
+	def index
 		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
 		@comments = Comment.where(post_id: @post)
 		@random_post = Post.where.not(id: @post).order("RANDOM()").first
+
 	end
 
 	def new
@@ -46,7 +48,7 @@ class PostsController < ApplicationController
 	end
 
 	def downvote
-		@post.downvote_by current_user
+		@post.downvote_from current_user
 		redirect_to :back
 	end
 
